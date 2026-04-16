072be7629634d7fea1944a1f89758860f718433f
Ammara Gul <139876497+Ammara-aziz@users.noreply.github.com>
2026-04-17 04:39:18 +0500

    Refactor SelectionSort to accept user input

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,39 +1,52 @@
 import java.util.Scanner;
+
 public class SelectionSort {
-    public static void main(String[] args){
-        int[] arr = {56,62,17,89,34};
-        System.out.print("Original Array: ");
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        getInput(sc, arr, n);
+
+        System.out.print("Original array: ");
         printArray(arr);
 
+        System.out.print("Sorted array: ");
         selectionSort(arr);
-
-        System.out.print("Sorted Array: ");
         printArray(arr);
     }
-    static void selectionSort(int[] arr){
-        int n = arr.length;
-        // One by one move the boundary of the unsorted subarray
-        for (int i = 0; i < n-1; i++) {
-        // Outer loop: i goes from 0 to n-1
-        // At each step, arr[0..i-1] is sorted, arr[i..n-1] is unsorted
+
+    static void getInput(Scanner sc, int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    static void selectionSort(int[] arr) {
+        int x = arr.length;
+
+        for (int i = 0; i < x - 1; i++) {
             int min_index = i;
 
-            for (int j = i+1 ; j < n; j++) {
-                if (arr[j]< arr[min_index]){
-                    min_index = j; // update index of minimum element
-
+            for (int j = i + 1; j < x; j++) {
+                if (arr[j] < arr[min_index]) {   // compare values
+                    min_index = j;
                 }
             }
-            // Swap minimum element with arr[i]
-            // This moves the smallest remaining element to its correct position
+
+            // Swap arr[i] and arr[min_index]
             int temp = arr[i];
             arr[i] = arr[min_index];
             arr[min_index] = temp;
         }
     }
-    static void printArray(int[] arr){
-        for (int val:arr){
-            System.out.print(val + " ");
+
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
         }
         System.out.println();
     }
